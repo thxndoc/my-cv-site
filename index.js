7a4a810ceffd101211926c10a7dac8015e912e34
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-04-20 17:41:01 +0200

    Change lon to longitude and lat to latitude: that is what open-meteo API uses

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const weather = document.getElementById("weather");
 
 navigator.geolocation.getCurrentPosition(async position => {
     try {
-        const response = await fetch (`https://api.open-meteo.com/v1/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        const response = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&current_weather=true&units=metric`)
         if (!response.ok) {
             throw Error ("Weather data not available")
         }
-        const data = await response.json()
+        const data = await response.json();
+        console.log(data.current_weather);
        
     } catch (err) {
-        console.error(err)
+        console.error(err);
     }
 });
