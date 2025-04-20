82221a32d3274782f72bafbc502042088ad3d310
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-04-20 18:50:26 +0200

    Display location and weather on browser

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const weather = document.getElementById("weather");
+const location = document.getElementById("location");
 
 navigator.geolocation.getCurrentPosition(async position => {
     try {
-        const response = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&current_weather=true&units=metric`)
-        if (!response.ok) {
+        const weatherResponse = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&current_weather=true&units=metric`)
+        if (!weatherResponse.ok) {
             throw Error ("Weather data not available")
         }
-        const data = await response.json();
-        //console.log(data.current_weather);
+        const weatherData = await weatherResponse.json();
+        //console.log(weatherData.current_weather);
 
-        // display weather on screen
-        weather.textContent = `Current weather is: ${Math.round(data.current_weather.temperature)}°C`
-       
+        // display name of area
+        const locationResponse = await fetch (`https://nominatim.openstreetmap.org/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`)
+        if (!locationResponse.ok) {
+            throw Error ("Location data not available")
+        }
+        const locationData = await locationResponse.json();
+        //console.log (locationData);
+
+        // display results
+        weather.textContent = 
+        `Current weather is: ${Math.round(weatherData.current_weather.temperature)}°C`;
+
+        location.textContent =
+        `Location: ${locationData.address.city}`;
+
     } catch (err) {
         console.error(err);
     }
